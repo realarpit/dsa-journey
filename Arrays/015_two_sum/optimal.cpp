a0cf6c747176114b9da18e0763cf47c0d49b48c9
Arpit Jaiswal <thearpitd3v@gmail.com>
2026-07-24 01:01:07 +0530

    Add two-sum solution with two-pointer approach
    Implemented two-sum algorithm using two-pointer technique.

--- Arrays/015_two_sum/optimal.cpp
+++ Arrays/015_two_sum/optimal.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int arr[] = {1,2,5,7,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target=14;
+    bool found = false;
+    
+    int left=0, right=n-1;
+    sort(arr, arr + n);
+    while(left<right){
+        int sum = arr[left]+arr[right];
+        if(sum==target){
+            found = true;
+            break;
+        }
+        else if(sum<target){
+            left++;
+        }
+        else right--;
+    }
+    if (!found) {
+        cout << "No";
+    }
+}
