0a88d863c2118b5e4d6aca78dde2250144cfcb7c
Arpit Jaiswal <thearpitd3v@gmail.com>
2026-07-24 09:38:08 +0530

    Implement two-sum algorithm using a map

--- Arrays/015_two_sum/better.cpp
+++ Arrays/015_two_sum/better.cpp
@@ -1,1 +1,27 @@
-
+#include <iostream>
+#include <map>
+using namespace std;
+int main() {
+    int arr[]={1,3,4,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 10;
+    bool found = false;
+    map<int,int>mpp;
+    
+    for (int i = 0; i < n; i++) {
+    int a = arr[i];
+    int more = target - a;
+    
+    if(mpp.find(more) != mpp.end()){
+        found = true;
+        break;
+    }
+    mpp[a] = i;
+    }
+    if(!found){
+        cout<<"no";
+  }
+  else{
+      cout<<"yes";
+  }
+}
