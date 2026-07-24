a295822ed2bff6ece8fc06e6b4dc30d18662ca2e
Arpit Jaiswal <thearpitd3v@gmail.com>
2026-07-24 17:20:22 +0530

    Add sorting algorithm for array of 0s, 1s, and 2s
    Implement sorting of an array containing 0s, 1s, and 2s using counting.

---
+++ 016_sortArray_of_0,1,2/better.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={0,1,1,2,1,2,0,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    int c0=0,c1=0,c2=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0) c0++;
+        else if(arr[i]==1) c1++;
+        else c2++;
+    }
+    for(int i=0;i<c0;i++) arr[i]=0;
+    for(int i=c0;i<c0+c1;i++) arr[i]=1;
+    for(int i=c0+c1;i<n;i++) arr[i]=2;\
+    
+    for(auto it:arr){
+        cout<<it<<" ";
+    }
+}
