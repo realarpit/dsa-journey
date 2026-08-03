2b7c855e2c322293c2179fb3d05a23153ee27aad
Arpit Jaiswal <thearpitd3v@gmail.com>
2026-08-03 08:59:20 +0530

    Add optimal solution for rearranging array by sign
    Implement rearrangement of array by sign.

---
+++ Arrays/020_rearrageArraybySign/optimal.cpp
@@ -0,0 +1,17 @@
+int main() {
+    int n = arr.size();
+    vector<int> ans(n,0); //arr of size n having value 0
+    
+    int posIndex=0, negIndex=1;
+    
+    for(int i=0;i<n;i++){
+        if(nums[i]<0){
+            ans[negIndex] = arr[i];  //if arr elements less than 0 then put them at negative indexes
+            negIndex += 2;
+        }
+        else{
+            ans[posIndex] = arr[i];
+            posIndex += 2;
+        }
+    }
+}
